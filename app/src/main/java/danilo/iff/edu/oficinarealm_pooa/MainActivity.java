eb082e1893e77b88c75c91c88238db86f113927d
odanilomoreira <danilokof7@gmail.com>
2018-03-19 08:06:03 -0300

    primeiro commit

---
+++ app/src/main/java/danilo/iff/edu/oficinarealm_pooa/MainActivity.java
@@ -0,0 +1,13 @@
+package danilo.iff.edu.oficinarealm_pooa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
